47cd6d41c84ef4670ab625df0024219290951788
ThienpontJoran <Joran.Thienpont@student.howest.be>
2026-03-02 00:12:39 +0100

    feat: add BaseChessPiece and Pawn implementation

---
+++ pieces.py
@@ -0,0 +1,36 @@
+from abc import ABC, abstractmethod
+
+class BaseChessPiece(ABC):
+
+    def __init__(self, color: str, name: str, symbol: str, identifier: int):
+        self.color = color
+        self.name = name
+        self.symbol = symbol
+        self.identifier = identifier
+        self.position = None
+        self.is_alive = True
+        self.board = None
+
+
+    @abstractmethod
+    def move(self, movement):
+        print(movement)
+
+    def die(self):
+        self.is_alive = False
+
+    def __str__(self):
+        return f"{self.color} {self.name} {self.identifier}"
+
+    def __repr__(self):
+        return f"{self.color} {self.name} {self.identifier}"
+    
+
+class Pawn(BaseChessPiece):
+
+    def __init__(self, color:str, identifier:int):
+        super().__init__(color, "Pawn", "-", identifier)
+
+    def move(self):
+        movement = "Pawn moves forward 1 position"
+        super().move(movement)
