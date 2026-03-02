cdb3d7c2d1f8b939c8b9af9d6f42a526c609c575
ThienpontJoran <Joran.Thienpont@student.howest.be>
2026-03-02 23:34:54 +0100

    feat: implement Board class and setup initial chess pieces

--- main.py
+++ main.py
@@ -1,15 +1,5 @@
-from pieces import Pawn, Rook, Bishop, Knight, Queen, King
+from pieces import Pawn, Rook, Bishop, Knight, Queen, King, Board
 
-pawn = Pawn("BLACK", 1)
-rook = Rook("BLACK", 1)
-bishop = Bishop("BLACK", 1)
-knight = Knight("BLACK", 1)
-queen = Queen("BLACK", 1)
-king = King("BLACK", 1)
 
-pawn.move()
-rook.move()
-bishop.move()
-knight.move()
-queen.move()
-king.move()
+board = Board()
+board.print_board()
--- pieces.py
+++ pieces.py
@@ -85,3 +85,63 @@ class King(BaseChessPiece):
     def move(self):
         movement = "King moves one square in any direction"
         super().move(movement)
+
+
+
+
+class Board:
+
+    def __init__(self):
+        self.squares = {
+            f"{chr(col)}{row}": None
+            for col in range(ord('a'), ord('i'))
+            for row in range(1, 9)
+        }
+
+        self.setup_board()
+
+    def print_board(self):
+        for row in range(1, 9):
+            row_squares = [
+                self.squares[f"{chr(col)}{row}"]
+                for col in range(ord('a'), ord('i'))
+            ]
+            print(row_squares)
+
+    def setup_board(self):
+
+        # BLACK major pieces (row 1)
+        self.squares['a1'] = Rook('BLACK', 1)
+        self.squares['b1'] = Knight('BLACK', 1)
+        self.squares['c1'] = Bishop('BLACK', 1)
+        self.squares['d1'] = Queen('BLACK', 1)
+        self.squares['e1'] = King('BLACK', 1)
+        self.squares['f1'] = Bishop('BLACK', 2)
+        self.squares['g1'] = Knight('BLACK', 2)
+        self.squares['h1'] = Rook('BLACK', 2)
+
+        # BLACK pawns (row 2)
+        black_pawns = {
+            f"{chr(col)}2": Pawn('BLACK', col - ord('a') + 1)
+            for col in range(ord('a'), ord('i'))
+        }
+
+        self.squares.update(black_pawns)
+
+        # WHITE major pieces (row 8)
+        self.squares['a8'] = Rook('WHITE', 1)
+        self.squares['b8'] = Knight('WHITE', 1)
+        self.squares['c8'] = Bishop('WHITE', 1)
+        self.squares['d8'] = Queen('WHITE', 1)
+        self.squares['e8'] = King('WHITE', 1)
+        self.squares['f8'] = Bishop('WHITE', 2)
+        self.squares['g8'] = Knight('WHITE', 2)
+        self.squares['h8'] = Rook('WHITE', 2)
+
+        # WHITE pawns (row 7)
+        white_pawns = {
+            f"{chr(col)}7": Pawn('WHITE', col - ord('a') + 1)
+            for col in range(ord('a'), ord('i'))
+        }
+
+        self.squares.update(white_pawns)
